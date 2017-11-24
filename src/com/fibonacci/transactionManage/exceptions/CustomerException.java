fde48d10a51bd5ac13aaa1e34aa683c79c34cd56
Vikas Thakur <vithakur87@gmail.com>
2017-11-25 01:46:05 +0530

    Create CustomerException.java

---
+++ src/com/fibonacci/transactionManage/exceptions/CustomerException.java
@@ -0,0 +1,32 @@
+package com.fibonacci.transactionManage.exceptions;
+
+public class CustomerException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public CustomerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomerException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+}
